83c6483a341acfd137718d46f056e3c7a114a5d0
Oktav <87888333+PutraOktav@users.noreply.github.com>
2024-06-12 13:29:04 +0700

    f

--- resources/views/livewire/Produk-Lengkap.blade.php
+++ resources/views/livewire/Produk-Lengkap.blade.php
@@ -1,16 +1,17 @@
 <div>
-    <section
-        class="grid mb-10 bg-gray-700 text-white text-center bg-cover z-10 relative bg-[url('https://live.staticflickr.com/65535/49909538937_3255dcf9e7_b.jpg')]">
-        <div class="w-full h-full col-start-1 row-start-1 bg-gray-800 bg-opacity-40"></div>
-        <div class="col-start-1 row-start-1 px-10 py-24">
-            <h1 class="py-5 text-4xl font-bold leading-none tracking-tight underline underline-offset-8">TRAINER KAMI
-            </h1>
-        </div>
-    </section>
+    <div>
+        <section
+            class="grid mb-10 bg-gray-700 text-white text-center bg-cover z-10 relative bg-[url('https://live.staticflickr.com/65535/49909538937_3255dcf9e7_b.jpg')]">
+            <div class="w-full h-full col-start-1 row-start-1 bg-gray-800 bg-opacity-40"></div>
+            <div class="col-start-1 row-start-1 px-10 py-24">
+                <h1 class="py-5 text-4xl font-bold leading-none tracking-tight underline underline-offset-8">TRAINER KAMI
+                </h1>
+            </div>
+        </section>
 
-    <section class="mb-4 flex justify-center items-center">
-        <input type="text" wire:model.live="search" placeholder="Search for Anything..."
-            class="px-4 py-2 placeholder:italic border rounded-lg h-16  w-1/4 fill-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1">
+        <section class="mb-4 flex justify-center items-center">
+            <input type="text" wire:model.live="search" placeholder="Search for Anything..."
+                class="px-4 py-2 placeholder:italic border rounded-lg h-16  w-1/4 fill-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1">
     </div>
 
     <div class="flex items-center justify-center mx-5">
@@ -31,8 +32,8 @@
                         class="odd:bg-white even:bg-slate-100 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-100 hover:bg-sky-400 duration-200 cursor-pointer">
                         <td class="px-2 py-4 border-2">{{ $loop->iteration }}</td>
                         <td class="px-3 py-4 border-2 font-bold md:px-1">
-                            <a href="{{ route('produk.detail', ['idbarang' => $barang->idbarang]) }}">{{
-                                $barang->idbarang }}</a>
+                            <a
+                                href="{{ route('produk.detail', ['idbarang' => $barang->idbarang]) }}">{{ $barang->idbarang }}</a>
                         </td>
                         <td class="px-6 py-4 border-2 font-bold md:px-3">
                             <a href="{{ route('produk.detail', ['idbarang' => $barang->idbarang]) }}">
@@ -41,19 +42,18 @@
                             </a>
                         </td>
                         <td class="px-6 py-4 border-2 md:px-3">
-                            <a href="{{ route('produk.detail', ['idbarang' => $barang->idbarang]) }}">{{
-                                $barang->jenis_barang }}</a>
+                            <a
+                                href="{{ route('produk.detail', ['idbarang' => $barang->idbarang]) }}">{{ $barang->jenis_barang }}</a>
                         </td>
                         <td class="px-6 py-4 border-2 md:px-2 md:whitespace-normal ">
-                            <a href="{{ route('produk.detail', ['idbarang' => $barang->idbarang]) }}">{{ $barang->nama
-                                }}</a>
+                            <a
+                                href="{{ route('produk.detail', ['idbarang' => $barang->idbarang]) }}">{{ $barang->nama }}</a>
                         </td>
                     </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
     </section>
-
 
 </div>
