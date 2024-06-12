1cd72abf720e870fc7c855125b49540bab8f33a0
afwanfari <afwanfarai00@gmail.com>
2024-06-12 14:41:15 +0700

    add

--- app/Filament/Resources/GaleriResource.php
+++ app/Filament/Resources/GaleriResource.php
@@ -67,8 +67,8 @@ class GaleriResource extends Resource
     public static function getRelations(): array
     {
         return [
-                //
-            ];
+            //
+        ];
     }
 
     public static function getPages(): array
